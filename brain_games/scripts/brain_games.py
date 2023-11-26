7ddb6de0622d57fba7ae99aab461a90445f3a628
Aleх Bark <thrtth@yandex.ru>
2023-11-26 12:34:28 +0700

    Step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
 def main():
     print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print(f'Hello, {name}!')
 
 
 if __name__ == '__main__':
